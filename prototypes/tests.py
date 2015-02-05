88afd09858aabe76ce32c271b2089eb9dcc99813
Preom Rahman <preom.m.r@gmail.com>
2015-02-05 08:53:30 -0500

    Added chatbot skeleton and test file

---
+++ prototypes/tests.py
@@ -0,0 +1,20 @@
+import unittest
+from chatbot import Chatbot, Alice, ChatbotFactory
+
+class ChatbotTest(unittest.TestCase):
+	def test_smoke_test(self):
+		chatbot = Chatbot()
+
+class AliceTest(unittest.TestCase):
+	def test_smoke_test(self):
+		alice = Alice()
+
+		bot_factory = ChatbotFactory()
+		alice_copy = bot_factory.create_chatbot(name='alice')
+
+		self.assertTrue(isinstance(alice_copy, Alice))
+
+
+if __name__ == '__main__':
+	unittest.main()
+
