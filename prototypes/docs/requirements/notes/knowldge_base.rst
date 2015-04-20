1612c15949c6b6a85d4290c37e366fec649c077c
Preom Rahman <preom.m.r@gmail.com>
2015-04-20 11:35:57 -0400

    Add more classes to knowledge base system and add some more core functionality

--- prototypes/docs/requirements/notes/knowldge_base.rst
+++ prototypes/docs/requirements/notes/knowldge_base.rst
@@ -3,3 +3,9 @@ Knowledge Base
 
 * Should use some kind of pattern like dependency injection to avoid the 
 difficulties of the unclear organization of a knoweldge base system.
+
+* scripts to populate knowledge base
+
+* procedurally populate data
+
+* resolve issues 
--- prototypes/knowledge_base/knowledge_base.py
+++ prototypes/knowledge_base/knowledge_base.py
@@ -1,6 +1,6 @@
 class KnowledgeBase(object):
     def all(self):
-        return KB.get_children()
+        return KbRoot.get_children()
 
     def match(self, pattern):
 
@@ -20,48 +20,93 @@ class KnowledgeBase(object):
         return results
 
 
-class KB(object):
+class KbRoot(object):
+    def __init__(self):
+        self.category = None
+        self.bull = 'hello'
+        self.alt_spelling = []
+
+    def get_word_class(self):
+        return getattr(self.__class__, '_word_class')
+
+    def get_name(self):
+        return self.__class__.__name__
 
     @staticmethod
     def get_children():
         def getc(cls):
                 l = [(cls.__name__, cls)]
+                alt_attr = "_" + cls.__class__.__name__ + '.__alts'
+                if hasattr(cls, alt_attr):
+                    l.extend([(wrd, cls) for wrd in cls.getattr(alt_attr)])
                 for i in [getc(c) for c in cls.__subclasses__()]:
                     l.extend(i)
 
                 return l
 
-        result = getc(KB)[1:] # Disclude "KB" class
+        result = getc(KbRoot)[1:] # Disclude "KB" class
         return result
 
 
-class Animal(Noun):
+class Animal(KbRoot):
+    _word_class = 'noun'
     def __init__(self):
         pass
-
 
 class Dog(Animal):
+    _domesticated = True
+
     def __init__(self):
-        pass
+        super(Dog, self).__init__()
 
 
 class GSD(Dog):
     def __init__(self):
-        pass
+        super(GSD, self).__init__()
 
 
 class chiuaha(Dog):
     def __init__(self):
-        pass
+        super(chiuaha, self).__init__()
 
 
 class Goat(Animal):
-    def __init__(self):
-        pass
+    _word_class = 'verb'
 
+    def __init__(self):
+        super(Goat, self).__init__()
+
+class Verb(KbRoot):
+    def __init__(self):
+        super(Verb, self).__init__()
+
+class Play(Verb)
+    _word_class = 'verb'
+    def __init__(self):
+        super(Play, self)
+
+class Ask(Verb)
+    _word_class = 'verb'
+    def __init__(self):
+        super(Ask, self)
 
 if __name__ == "__main__":
     kb = KnowledgeBase()
     print kb.all()
 
+
+    dog = Dog()
+    goat = Goat()
+    animal = Animal()
+
+    dir(dog)
+    dir(goat)
+    dir(animal)
+
     print kb.match('dog')
+    d = kb.match('dog')[0]()
+    g = kb.match('goat')[0]()
+    print d.get_word_class()
+    print g.get_word_class(), g.get_name()
+
+    print dir(g)
